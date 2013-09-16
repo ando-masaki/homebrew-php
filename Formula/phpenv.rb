9b51044a4842455c570274aa5a8b91d4f1c25692
ando-masaki <ando_masaki@cygames.co.jp>
2013-09-16 22:42:55 +0900

    Canged phpenv.rb to use phpenv/phpenv.

--- Formula/phpenv.rb
+++ Formula/phpenv.rb
@@ -1,26 +1,21 @@
 require 'formula'
 
 class Phpenv < Formula
-  homepage 'https://github.com/humanshell/phpenv'
-  url 'https://github.com/humanshell/phpenv/archive/c2e51e51427751431b0b322722a3bb9b6c0061bc.tar.gz'
-  sha1 '2f8e75e3a101edc66e1e2439c906363d25afe9de'
-  head 'https://github.com/humanshell/phpenv.git'
-  version 'c2e51e5'
+  homepage 'https://github.com/phpenv/phpenv'
+  url 'https://github.com/phpenv/phpenv/archive/00a7c8af25d96974841bf096103e28480837de33.tar.gz'
+  sha1 '099f16dbdc13d71e590abd7390af37d00b234eea'
+  head 'https://github.com/phpenv/phpenv.git'
+  version '099f16d'
 
   def install
-    inreplace 'libexec/phpenv', '/usr/local', HOMEBREW_PREFIX
-    prefix.install Dir['*']
+    prefix.install 'bin', 'phpenv.d', 'completions', 'libexec'
+    # Run rehash after installing.
+    system "#{bin}/phpenv", "rehash"
   end
 
   def caveats; <<-EOS.undent
-    To finish installing phpenv:
-
-      * Add phpenv init to your shell to enable shims and autocompletion.
-        $ echo 'eval "$(phpenv init -)"' >> ~/.bash_profile
-      * Restart your shell so the path changes take effect. You can now begin using phpenv.
-        $ exec $SHELL
-      * Rebuild the shim binaries. You should do this any time you install a new PHP binary.
-        $ phpenv rehash
+    To enable shims add to your profile:
+      eval "$(phpenv init -)"
     EOS
   end
 end
